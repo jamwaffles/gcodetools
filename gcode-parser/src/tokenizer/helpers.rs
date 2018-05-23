103d6c77da7237d96454c56278ab192487645ecb
James Waples <jamwaffles@gmail.com>
2018-05-23 22:21:48 +0100

    Parse %-delimited program

--- gcode-parser/src/tokenizer/helpers.rs
+++ gcode-parser/src/tokenizer/helpers.rs
@@ -1,6 +1,37 @@
 use nom::types::CompleteByteSlice;
 use nom::*;
 
+#[derive(Debug, PartialEq)]
+pub struct Vec9 {
+    pub x: Option<f32>,
+    pub y: Option<f32>,
+    pub z: Option<f32>,
+    pub a: Option<f32>,
+    pub b: Option<f32>,
+    pub c: Option<f32>,
+    pub u: Option<f32>,
+    pub v: Option<f32>,
+    pub w: Option<f32>,
+    pub r: Option<f32>,
+}
+
+impl Default for Vec9 {
+    fn default() -> Vec9 {
+        Vec9 {
+            x: None,
+            y: None,
+            z: None,
+            a: None,
+            b: None,
+            c: None,
+            u: None,
+            v: None,
+            w: None,
+            r: None,
+        }
+    }
+}
+
 named_args!(
     pub preceded_f32<'a>(preceding: &str)<CompleteByteSlice<'a>, f32>,
     flat_map!(preceded!(tag_no_case!(preceding), recognize!(recognize_float)), parse_to!(f32))
@@ -11,6 +42,39 @@ named_args!(
     flat_map!(preceded!(tag_no_case!(preceding), recognize!(preceded!(opt!(one_of!("+-")), digit))), parse_to!(i32))
 );
 
+named_args!(
+    pub preceded_u32<'a>(preceding: &str)<CompleteByteSlice<'a>, u32>,
+    flat_map!(preceded!(tag_no_case!(preceding), recognize!(digit)), parse_to!(u32))
+);
+
+named!(
+    pub vec9<CompleteByteSlice, Vec9>,
+    ws!(do_parse!(
+        x: opt!(call!(preceded_f32, "X")) >>
+        y: opt!(call!(preceded_f32, "Y")) >>
+        z: opt!(call!(preceded_f32, "Z")) >>
+        a: opt!(call!(preceded_f32, "A")) >>
+        b: opt!(call!(preceded_f32, "B")) >>
+        c: opt!(call!(preceded_f32, "C")) >>
+        u: opt!(call!(preceded_f32, "U")) >>
+        v: opt!(call!(preceded_f32, "V")) >>
+        w: opt!(call!(preceded_f32, "W")) >>
+        r: opt!(call!(preceded_f32, "R")) >>
+        (Vec9 {
+            x,
+            y,
+            z,
+            a,
+            b,
+            c,
+            u,
+            v,
+            w,
+            r,
+        })
+    ))
+);
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -19,7 +83,62 @@ mod tests {
     const EMPTY: Cbs = Cbs(b"");
 
     #[test]
+    fn it_parses_vectors() {
+        assert_eq!(
+            vec9(Cbs(b"X0 Y1 Z2")),
+            Ok((
+                EMPTY,
+                Vec9 {
+                    x: Some(0.0f32),
+                    y: Some(1.0f32),
+                    z: Some(2.0f32),
+                    ..Default::default()
+                }
+            ))
+        );
+
+        assert_eq!(
+            vec9(Cbs(b"X-0.5 Y-2 Z100")),
+            Ok((
+                EMPTY,
+                Vec9 {
+                    x: Some(-0.5f32),
+                    y: Some(-2.0f32),
+                    z: Some(100.0f32),
+                    ..Default::default()
+                }
+            ))
+        );
+
+        assert_eq!(
+            vec9(Cbs(b"Z1")),
+            Ok((
+                EMPTY,
+                Vec9 {
+                    z: Some(1.0f32),
+                    ..Default::default()
+                }
+            ))
+        );
+
+        assert_eq!(
+            vec9(Cbs(b"U2.5 V3.5 W4.5")),
+            Ok((
+                EMPTY,
+                Vec9 {
+                    u: Some(2.5f32),
+                    v: Some(3.5f32),
+                    w: Some(4.5f32),
+                    ..Default::default()
+                }
+            ))
+        );
+    }
+
+    #[test]
     fn it_parses_preceded_floats() {
+        assert_eq!(preceded_f32(Cbs(b"x1."), "X"), Ok((EMPTY, 1.0f32)));
+
         assert_eq!(preceded_f32(Cbs(b"x1.23"), "X"), Ok((EMPTY, 1.23f32)));
         assert_eq!(preceded_f32(Cbs(b"y-1.23"), "Y"), Ok((EMPTY, -1.23f32)));
         assert_eq!(preceded_f32(Cbs(b"z+1.23"), "Z"), Ok((EMPTY, 1.23f32)));
@@ -38,5 +157,14 @@ mod tests {
 
         assert_eq!(preceded_i32(Cbs(b"X123"), "X"), Ok((EMPTY, 123i32)));
         assert_eq!(preceded_i32(Cbs(b"Y-123"), "Y"), Ok((EMPTY, -123i32)));
+    }
+
+    #[test]
+    fn it_parses_preceded_unsigned_integers() {
+        assert_eq!(preceded_u32(Cbs(b"x123"), "X"), Ok((EMPTY, 123u32)));
+        assert_eq!(preceded_u32(Cbs(b"X123"), "X"), Ok((EMPTY, 123u32)));
+
+        assert!(preceded_u32(Cbs(b"y-123"), "Y").is_err());
+        assert!(preceded_u32(Cbs(b"Y-123"), "Y").is_err());
     }
 }
--- gcode-parser/src/tokenizer/mod.rs
+++ gcode-parser/src/tokenizer/mod.rs
@@ -40,13 +40,39 @@ pub enum CutterCompensation {
 }
 
 #[derive(Debug, PartialEq)]
+pub enum Plane {
+    Xy,
+    Zx,
+    Yz,
+    Uv,
+    Wu,
+    Vw,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum SpindleRotation {
+    Cw,
+    Ccw,
+    Stop,
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Token {
     Comment(String),
     Units(Units),
     DistanceMode(DistanceMode),
     PathBlending(PathBlending),
     CutterCompensation(CutterCompensation),
+    RapidMove(Vec9),
+    LinearMove(Vec9),
+    ToolSelect(u32),
+    ToolChange,
+    PlaneSelect(Plane),
+    SpindleRotation(SpindleRotation),
+    SpindleSpeed(i32),
 }
+
+pub type Program = Vec<Token>;
 
 named!(comment<CompleteByteSlice, Token>, map!(
     flat_map!(
@@ -91,6 +117,74 @@ named!(cutter_compensation<CompleteByteSlice, Token>,
     )
 );
 
+named!(rapid_move<CompleteByteSlice, Token>,
+    map!(ws!(preceded!(tag!("G0"), vec9)), |res| Token::RapidMove(res))
+);
+
+named!(linear_move<CompleteByteSlice, Token>,
+    map!(ws!(preceded!(tag!("G1"), vec9)), |res| Token::LinearMove(res))
+);
+
+named!(tool_number<CompleteByteSlice, Token>,
+    map!(call!(preceded_u32, "T"), |res| Token::ToolSelect(res))
+);
+
+named!(tool_change<CompleteByteSlice, Token>,
+    map!(tag!("M6"), |_| Token::ToolChange)
+);
+
+named!(plane_select<CompleteByteSlice, Token>, map!(
+    alt!(
+        map!(tag_no_case!("G17.1"), |_| Plane::Uv) |
+        map!(tag_no_case!("G18.1"), |_| Plane::Wu) |
+        map!(tag_no_case!("G19.1"), |_| Plane::Vw) |
+        map!(tag_no_case!("G17"), |_| Plane::Xy) |
+        map!(tag_no_case!("G18"), |_| Plane::Zx) |
+        map!(tag_no_case!("G19"), |_| Plane::Yz)
+    ),
+    |res| Token::PlaneSelect(res)
+));
+
+named!(spindle_rotation<CompleteByteSlice, Token>, map!(
+    alt!(
+        map!(tag_no_case!("M3"), |_| SpindleRotation::Cw) |
+        map!(tag_no_case!("M4"), |_| SpindleRotation::Ccw) |
+        map!(tag_no_case!("M5"), |_| SpindleRotation::Stop)
+    ),
+    |res| Token::SpindleRotation(res)
+));
+
+named!(spindle_speed<CompleteByteSlice, Token>, map!(
+    call!(preceded_i32, "S"),
+    |res| Token::SpindleSpeed(res)
+));
+
+named!(token<CompleteByteSlice, Token>,
+    alt_complete!(
+        comment |
+        units |
+        distance_mode |
+        path_blending |
+        cutter_compensation |
+        rapid_move |
+        linear_move |
+        tool_number |
+        tool_change |
+        plane_select |
+        spindle_rotation |
+        spindle_speed
+    )
+);
+
+named!(tokens<CompleteByteSlice, Vec<Token>>, ws!(many0!(token)));
+
+named!(program<CompleteByteSlice, Program>,
+    ws!(delimited!(tag!("%"), many0!(token), tag!("%")))
+);
+
+// Note: programs are either dlimited by % signs or stop at M2/M30. Anything after a trailing %/M2/
+// M30 MUST be ignored
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -104,6 +198,110 @@ mod tests {
         against: Token,
     ) {
         assert_eq!(to_check, Ok((EMPTY, against)))
+    }
+
+    #[test]
+    fn it_parses_a_percent_delimited_program() {
+        let percents = r#"%
+G21
+G0 x0 y0 z0
+G1 Z10
+%
+G0 Z10
+"#;
+
+        let percents_program = program(Cbs(percents.as_bytes()));
+
+        assert_eq!(
+            percents_program,
+            Ok((
+                // Ignore anything after last %
+                Cbs(b"G0 Z10\n"),
+                vec![
+                    Token::Units(Units::Mm),
+                    Token::RapidMove(Vec9 {
+                        x: Some(0.0),
+                        y: Some(0.0),
+                        z: Some(0.0),
+                        ..Default::default()
+                    }),
+                    Token::LinearMove(Vec9 {
+                        z: Some(10.0),
+                        ..Default::default()
+                    }),
+                ]
+            ))
+        );
+    }
+
+    #[test]
+    fn it_parses_spindle_speed() {
+        check_token(spindle_speed(Cbs(b"S0")), Token::SpindleSpeed(0i32));
+        check_token(spindle_speed(Cbs(b"S1000")), Token::SpindleSpeed(1000i32));
+        check_token(spindle_speed(Cbs(b"S-250")), Token::SpindleSpeed(-250i32));
+    }
+
+    #[test]
+    fn it_parses_spindle_rotation() {
+        check_token(
+            spindle_rotation(Cbs(b"M3")),
+            Token::SpindleRotation(SpindleRotation::Cw),
+        );
+        check_token(
+            spindle_rotation(Cbs(b"M4")),
+            Token::SpindleRotation(SpindleRotation::Ccw),
+        );
+        check_token(
+            spindle_rotation(Cbs(b"M5")),
+            Token::SpindleRotation(SpindleRotation::Stop),
+        );
+    }
+
+    #[test]
+    fn it_parses_plane_select() {
+        check_token(plane_select(Cbs(b"G17")), Token::PlaneSelect(Plane::Xy));
+        check_token(plane_select(Cbs(b"G18")), Token::PlaneSelect(Plane::Zx));
+        check_token(plane_select(Cbs(b"G19")), Token::PlaneSelect(Plane::Yz));
+        check_token(plane_select(Cbs(b"G17.1")), Token::PlaneSelect(Plane::Uv));
+        check_token(plane_select(Cbs(b"G18.1")), Token::PlaneSelect(Plane::Wu));
+        check_token(plane_select(Cbs(b"G19.1")), Token::PlaneSelect(Plane::Vw));
+    }
+
+    #[test]
+    fn it_changes_tool() {
+        check_token(tool_change(Cbs(b"M6")), Token::ToolChange);
+    }
+
+    #[test]
+    fn it_parses_tool_number() {
+        check_token(tool_number(Cbs(b"T0")), Token::ToolSelect(0u32));
+        check_token(tool_number(Cbs(b"T99")), Token::ToolSelect(99u32));
+    }
+
+    #[test]
+    fn it_parses_rapids() {
+        check_token(
+            rapid_move(Cbs(b"G0 X0 Y1 Z2")),
+            Token::RapidMove(Vec9 {
+                x: Some(0.0f32),
+                y: Some(1.0f32),
+                z: Some(2.0f32),
+                ..Default::default()
+            }),
+        );
+    }
+
+    #[test]
+    fn it_parses_linear_moves() {
+        check_token(
+            linear_move(Cbs(b"G1 X0 Y1 Z2")),
+            Token::LinearMove(Vec9 {
+                x: Some(0.0f32),
+                y: Some(1.0f32),
+                z: Some(2.0f32),
+                ..Default::default()
+            }),
+        );
     }
 
     #[test]
