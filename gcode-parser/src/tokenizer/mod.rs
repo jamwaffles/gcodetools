e2202a2769e3016c6c1b97b03483059f5e57a4ef
James Waples <jamwaffles@gmail.com>
2018-05-23 16:10:26 +0100

    Parse blending mode

--- gcode-parser/src/tokenizer/mod.rs
+++ gcode-parser/src/tokenizer/mod.rs
@@ -1,4 +1,5 @@
 use nom::types::CompleteByteSlice;
+use nom::*;
 
 pub struct Tokenizer {}
 
@@ -25,10 +26,17 @@ pub enum DistanceMode {
 }
 
 #[derive(Debug, PartialEq)]
+pub struct PathBlending {
+    pub p: Option<f32>,
+    pub q: Option<f32>,
+}
+
+#[derive(Debug, PartialEq)]
 pub enum Token {
     Comment(String),
     Units(Units),
     DistanceMode(DistanceMode),
+    PathBlending(PathBlending),
 }
 
 named!(comment<CompleteByteSlice, Token>, map!(
@@ -55,11 +63,75 @@ named!(distance_mode<CompleteByteSlice, Token>, map!(
     |res| Token::DistanceMode(res)
 ));
 
+// TODO: Move to helpers file
+named_args!(
+    pub preceded_f32<'a>(preceding: &str)<CompleteByteSlice<'a>, f32>,
+    flat_map!(preceded!(tag_no_case!(preceding), recognize!(recognize_float)), parse_to!(f32))
+);
+
+// TODO: Move to helpers file
+named_args!(
+    pub preceded_i32<'a>(preceding: &str)<CompleteByteSlice<'a>, i32>,
+    flat_map!(preceded!(tag_no_case!(preceding), recognize!(preceded!(opt!(one_of!("+-")), digit))), parse_to!(i32))
+);
+
+named!(path_blending<CompleteByteSlice, Token>, ws!(
+    do_parse!(
+        tag_no_case!("G64") >>
+        p: opt!(call!(preceded_f32, "P")) >>
+        q: opt!(call!(preceded_f32, "Q")) >> ({
+            Token::PathBlending(PathBlending { p, q })
+        })
+    )
+));
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     const EMPTY: CompleteByteSlice = CompleteByteSlice(b"");
+
+    #[test]
+    fn it_parses_blending_mode() {
+        assert_eq!(
+            path_blending(CompleteByteSlice(b"G64")),
+            Ok((
+                EMPTY,
+                Token::PathBlending(PathBlending { p: None, q: None })
+            ))
+        );
+
+        assert_eq!(
+            path_blending(CompleteByteSlice(b"G64 P0.01")),
+            Ok((
+                EMPTY,
+                Token::PathBlending(PathBlending {
+                    p: Some(0.01f32),
+                    q: None
+                })
+            ))
+        );
+
+        assert_eq!(
+            path_blending(CompleteByteSlice(b"G64 P0.01 Q0.02")),
+            Ok((
+                EMPTY,
+                Token::PathBlending(PathBlending {
+                    p: Some(0.01f32),
+                    q: Some(0.02f32)
+                })
+            ))
+        );
+
+        // TODO
+        // assert_eq!(
+        //     path_blending(CompleteByteSlice(b"G64 Q0.02")),
+        //     Ok((
+        //         EMPTY,
+        //         Token::PathBlending(PathBlending { p: None, q: None })
+        //     ))
+        // );
+    }
 
     #[test]
     fn it_parses_distance_mode() {
