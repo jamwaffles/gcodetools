5d7fb6f43194168a8bd39e7a2d5bebe5828913b4
James Waples <jamwaffles@gmail.com>
2018-05-27 22:06:06 +0100

    Add some more benches

--- gcode-parser/benches/tokenizer.rs
+++ gcode-parser/benches/tokenizer.rs
@@ -6,6 +6,7 @@ extern crate nom;
 use nom::types::CompleteByteSlice as Cbs;
 
 use criterion::Criterion;
+use gcode_parser::tokenizer::test_prelude::*;
 use gcode_parser::tokenizer::Tokenizer;
 
 /// Benchmark parsing a contrived, but real-world-ish example
@@ -30,10 +31,33 @@ fn parse_linear_program(c: &mut Criterion) {
         b.iter(|| {
             let tokenizer = Tokenizer::new_from_str(&program);
 
-            tokenizer.tokenize();
+            tokenizer.tokenize().unwrap();
         })
     });
 }
 
-criterion_group!(tokenizer, parse_linear_program);
+/// Parse a coordinate
+fn parse_vec9(c: &mut Criterion) {
+    c.bench_function("Parse 9-dimension vectors", |b| {
+        // An example coord written by a drunk programmer to test all code paths
+        let program = b"X0.1 y1.0z3.5 A 4 B5 c6 u7 V 8 W9";
+
+        b.iter(|| {
+            vec9(Cbs(program)).unwrap();
+        })
+    });
+}
+
+/// Parse a center format arc
+fn parse_arc(c: &mut Criterion) {
+    c.bench_function("Parse center format arcs", |b| {
+        let program = b"X5.0417Y1.9427Z10.00000I-0.3979J0.3028l1.23456789";
+
+        b.iter(|| {
+            center_format_arc(Cbs(program)).unwrap();
+        })
+    });
+}
+
+criterion_group!(tokenizer, parse_linear_program, parse_vec9, parse_arc);
 criterion_main!(tokenizer);
