e1722a9dac3309123d705e81522d7827a886a99b
James Waples <jamwaffles@gmail.com>
2018-06-04 23:54:10 +0100

    Add expression parse/evaluate benchmark

--- gcode-parser/src/tokenizer/test_prelude.rs
+++ gcode-parser/src/tokenizer/test_prelude.rs
@@ -1,7 +1,9 @@
 pub use super::arc::*;
 pub use super::comment::*;
+pub use super::expression::expression as parse_expression;
 pub use super::gcodes::*;
 pub use super::helpers::*;
 pub use super::mcodes::*;
 pub use super::othercodes::*;
+pub use super::parameter::*;
 pub use super::vec9::*;
