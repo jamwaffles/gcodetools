c3cb7575e14bfc582b46dfac48043dc230a65cbd
James Waples <jamwaffles@gmail.com>
2018-06-06 23:48:53 +0100

    Add vec9 parse benchmark

---
+++ gcode-parser/benches/vec9.rs
@@ -0,0 +1,31 @@
+#[macro_use]
+extern crate criterion;
+extern crate gcode_parser;
+extern crate nom;
+
+use criterion::Criterion;
+use gcode_parser::tokenizer::test_prelude::*;
+use nom::types::CompleteByteSlice as Cbs;
+use std::time::Duration;
+
+fn bench_vec9(c: &mut Criterion) {
+    c.bench_function_over_inputs(
+        "Parse multiple coordinate samples",
+        |b, input| {
+            b.iter(|| {
+                coord(Cbs(input.as_bytes())).unwrap();
+            })
+        },
+        vec!["X10 Y20 X30", "X0Y1Z2", "X-0.5 Y-2 Z100", "Z1"],
+    );
+}
+
+criterion_group!{
+    name = vec9;
+    config = Criterion::default()
+        .warm_up_time(Duration::from_millis(100))
+        .sample_size(100)
+        .measurement_time(Duration::from_millis(3000));
+    targets = bench_vec9
+}
+criterion_main!(vec9);
