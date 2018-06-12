c824f45748a8a26851f5ffb988eeae8e44fc3c3c
James Waples <jamwaffles@gmail.com>
2018-06-12 22:08:42 +0100

    Attempt at getting profiles working on WSL
    It didn't work

--- gcode-parser/analyse_profile.sh
+++ gcode-parser/analyse_profile.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 # Make sure the deps are installed:
+# Mac:
 # brew install go gprof2dot
+# Linux/WSL:
+# sudo apt-get install -y libgoogle-perftools-dev golang python graphviz python-pip libunwind-dev
+# pip install gprof2dot
+#
 # go get -u github.com/google/pprof
 
 BIN_PATH="$1"
@@ -9,9 +14,14 @@ BIN_PATH="$1"
 BIN_NAME=$(basename -- "$BIN_PATH")
 BIN_NAME="${BIN_NAME%.*}"
 
+echo "Pprof..."
 pprof --callgrind $BIN_PATH "./profiles/${BIN_NAME}.profile" > "./profiles/${BIN_NAME}.callgrind"
+echo "Gprof2dot..."
 gprof2dot --format=callgrind --output="./profiles/${BIN_NAME}.dot" "./profiles/${BIN_NAME}.callgrind"
+echo "Dot..."
 dot -Tpng "./profiles/${BIN_NAME}.dot" -o "./profiles/${BIN_NAME}.png"
 
+echo "Done"
+
 # MacOS only
-open "./profiles/${BIN_NAME}.png"
+# open "./profiles/${BIN_NAME}.png"
