4110c4b829e801e4fa2aa72ee1088077f14bda0a
James Waples <jamwaffles@gmail.com>
2018-06-13 15:44:58 +0100

    Test more gcodes in g() bench

--- gcode-parser/benches/helpers.rs
+++ gcode-parser/benches/helpers.rs
@@ -3,10 +3,10 @@ extern crate criterion;
 extern crate gcode_parser;
 extern crate nom;
 
-use nom::types::CompleteByteSlice as Cbs;
-
 use criterion::Criterion;
 use gcode_parser::tokenizer::test_prelude::*;
+use nom::types::CompleteByteSlice as Cbs;
+use std::time::Duration;
 
 fn bench_one_of_no_case(c: &mut Criterion) {
     c.bench_function("One of no case", |b| {
@@ -17,11 +17,15 @@ fn bench_one_of_no_case(c: &mut Criterion) {
 }
 
 fn bench_g_code(c: &mut Criterion) {
-    c.bench_function("G code", |b| {
-        b.iter(|| {
-            g(Cbs(b"G99"), 99.0).unwrap();
-        })
-    });
+    c.bench_function_over_inputs(
+        "G codes",
+        |b, input| {
+            b.iter(|| {
+                gcode(Cbs(input.as_bytes())).unwrap();
+            })
+        },
+        vec!["G0", "G1", "G21", "G49"],
+    );
 }
 
 fn bench_m_code(c: &mut Criterion) {
@@ -32,5 +36,12 @@ fn bench_m_code(c: &mut Criterion) {
     });
 }
 
-criterion_group!(helpers, bench_one_of_no_case, bench_g_code, bench_m_code);
+criterion_group!{
+    name = helpers;
+    config = Criterion::default()
+        .warm_up_time(Duration::from_millis(200))
+        .sample_size(100)
+        .measurement_time(Duration::from_millis(3000));
+    targets = bench_one_of_no_case, bench_g_code, bench_m_code
+}
 criterion_main!(helpers);
