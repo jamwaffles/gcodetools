b9c19b046aba39dfa20d0abbfa27d88e5205a566
James Waples <jamwaffles@gmail.com>
2018-06-13 14:39:15 +0100

    Tweak display params a bit

--- gcode-parser/analyse_profile.sh
+++ gcode-parser/analyse_profile.sh
@@ -15,9 +15,9 @@ BIN_NAME=$(basename -- "$BIN_PATH")
 BIN_NAME="${BIN_NAME%.*}"
 
 echo "Pprof..."
-pprof --callgrind $BIN_PATH "./profiles/${BIN_NAME}.profile" > "./profiles/${BIN_NAME}.callgrind"
+pprof --nodecount=500 --nodefraction=0.0001 --edgefraction=0.0001 --maxdegree=128 --alloc_space --callgrind $BIN_PATH "./profiles/${BIN_NAME}.profile" > "./profiles/${BIN_NAME}.callgrind"
 echo "Gprof2dot..."
-gprof2dot --format=callgrind --output="./profiles/${BIN_NAME}.dot" "./profiles/${BIN_NAME}.callgrind"
+gprof2dot --show-samples -n 0.01 -e 0.001 --format=callgrind --output="./profiles/${BIN_NAME}.dot" "./profiles/${BIN_NAME}.callgrind"
 echo "Dot..."
 dot -Tpng "./profiles/${BIN_NAME}.dot" -o "./profiles/${BIN_NAME}.png"
 
