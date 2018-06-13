49e98584932c28c39138b00d8a087001433f3f67
James Waples <jamwaffles@gmail.com>
2018-06-13 15:36:11 +0100

    Only focus on stuff in gcode_parser

--- gcode-parser/analyse_profile.sh
+++ gcode-parser/analyse_profile.sh
@@ -15,7 +15,7 @@ BIN_NAME=$(basename -- "$BIN_PATH")
 BIN_NAME="${BIN_NAME%.*}"
 
 echo "Pprof..."
-pprof --nodecount=500 --nodefraction=0.0001 --edgefraction=0.0001 --maxdegree=128 --alloc_space --callgrind $BIN_PATH "./profiles/${BIN_NAME}.profile" > "./profiles/${BIN_NAME}.callgrind"
+pprof --focus gcode_parser --nodecount=500 --nodefraction=0.0001 --edgefraction=0.0001 --maxdegree=128 --alloc_space --callgrind $BIN_PATH "./profiles/${BIN_NAME}.profile" > "./profiles/${BIN_NAME}.callgrind"
 echo "Gprof2dot..."
 gprof2dot --show-samples -n 0.01 -e 0.001 --format=callgrind --output="./profiles/${BIN_NAME}.dot" "./profiles/${BIN_NAME}.callgrind"
 echo "Dot..."
