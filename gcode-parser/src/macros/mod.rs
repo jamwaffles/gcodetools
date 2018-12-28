a645561e0e16b1eeb7c78166dfaf80c037b72206
James Waples <james@wapl.es>
2018-12-28 16:00:13 +0000

    Output failing line and position

--- gcode-parser/src/macros/mod.rs
+++ gcode-parser/src/macros/mod.rs
@@ -2,46 +2,52 @@
 #[macro_use]
 mod test_helpers;
 
-macro_rules! snip {
-    ($input:expr, $n:expr) => {{
-        let lines = $input.lines();
-        let num_lines = $input.lines().count();
+// macro_rules! snip {
+//     ($input:expr, $n:expr) => {{
+//         let lines = $input.lines();
+//         let num_lines = $input.lines().count();
 
-        if num_lines > $n {
-            // TODO: Fix clone
-            let start = lines.clone().take($n / 2).collect::<Vec<&str>>().join("\n");
-            let mut end = lines.rev().take($n / 2).collect::<Vec<&str>>();
+//         if num_lines > $n {
+//             // TODO: Fix clone
+//             let start = lines.clone().take($n / 2).collect::<Vec<&str>>().join("\n");
+//             let mut end = lines.rev().take($n / 2).collect::<Vec<&str>>();
 
-            end.reverse();
+//             end.reverse();
 
-            let end = end.join("\n");
+//             let end = end.join("\n");
 
-            format!("{}\n...\n{}", start, end)
-        } else {
-            lines.take($n).collect::<Vec<&str>>().join("\n")
-        }
-    }};
+//             format!("{}\n...\n{}", start, end)
+//         } else {
+//             lines.take($n).collect::<Vec<&str>>().join("\n")
+//         }
+//     }};
 
-    ($input:expr) => {
-        snip!($input, 10)
-    };
-}
+//     ($input:expr) => {
+//         snip!($input, 10)
+//     };
+// }
 
 #[macro_export]
 macro_rules! format_parse_error {
     ($remaining:expr, $e:expr, $input:expr) => {{
         let remaining = String::from_utf8($remaining.fragment.to_vec()).unwrap();
-        let input = String::from_utf8($input.fragment.to_vec()).unwrap();
+        // let input = String::from_utf8($input.fragment.to_vec()).unwrap();
+
+        let erroring_line = remaining.lines().next().unwrap();
 
         format!(
-            "Parser execution failed\n-- Test input (len {})\n{}\n\n-- Error type\n{:?}\n\n-- Remaining input (len {})\n{}\n",
-            input.len(),
-            snip!(input),
-            $e,
-            remaining.len(),
-            snip!(remaining)
+            r#"Parser execution failed at {}:{}
+
+{}
+{}╯"#,
+            $remaining.line,
+            $remaining.get_column(),
+            erroring_line,
+            (0..$remaining.get_column() - 1)
+                .map(|_| "─")
+                .collect::<String>()
         )
-    }}
+    }};
 }
 
 // TODO: Move to parsers/
