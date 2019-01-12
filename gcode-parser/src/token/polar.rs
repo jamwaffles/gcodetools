f213937b0c20ae99f5f43ea25f88b1b9f003e9d7
James Waples <james@wapl.es>
2019-01-12 15:47:53 +0000

    Allow optional components of polar coords

--- gcode-parser/src/token/polar.rs
+++ gcode-parser/src/token/polar.rs
@@ -8,23 +8,27 @@ use nom::*;
 #[derive(Debug, PartialEq, Clone)]
 pub struct PolarCoord {
     /// Distance from origin
-    pub distance: Value,
+    pub distance: Option<Value>,
     /// Angle, starting at 0 on positive X axis. Positive direction is counterclockwise
-    pub angle: Value,
+    pub angle: Option<Value>,
 }
 
 named_attr!(#[doc = "Parse a polar coordinate"],
     pub polar<Span, PolarCoord>,
-    map!(
+    map_opt!(
         sep!(
             space0,
             permutation!(
-                sep!(space0, preceded!(char_no_case!('@'), ngc_float_value)),
-                sep!(space0, preceded!(char_no_case!('^'), ngc_float_value))
+                sep!(space0, preceded!(char_no_case!('@'), ngc_float_value))?,
+                sep!(space0, preceded!(char_no_case!('^'), ngc_float_value))?
             )
         ),
-        |(distance, angle)| {
-            PolarCoord { distance, angle }
+        |(distance, angle): (Option<Value>, Option<Value>)| {
+            if distance.is_none() && angle.is_none() {
+                None
+            } else {
+                Some(PolarCoord { distance, angle })
+            }
         }
     )
 );
@@ -40,9 +44,24 @@ mod tests {
             parser = polar;
             input = span!(b"@.5 ^90");
             expected = PolarCoord {
-                distance: 0.5.into(),
-                angle: 90.0.into()
+                distance: Some(0.5.into()),
+                angle: Some(90.0.into())
             }
+        );
+    }
+
+    #[test]
+    fn parse_polar_optional() {
+        assert_parse!(
+            parser = polar;
+            input =
+                span!(b"@.5"),
+                span!(b"^90")
+            ;
+            expected =
+                PolarCoord { distance: Some(0.5.into()), angle: None },
+                PolarCoord { distance: None, angle: Some(90.0.into()) }
+            ;
         );
     }
 }
