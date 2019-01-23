5d024422f8a75356fd68bed0a5bd33a7e1185ef6
James Waples <james@wapl.es>
2019-01-23 13:44:09 +0000

    Remove crappy old parser

--- _gcode-parser/src/mcodes/m100_m199_user_command.rs
+++
@@ -1,9 +0,0 @@
-use nom::types::CompleteByteSlice;
-
-use super::super::helpers::*;
-use super::MCode;
-
-named!(pub user_command<CompleteByteSlice, MCode>, map!(
-    call!(preceded_code_range_inclusive, "M", 100, 199),
-    |command_number| MCode::UserCommand(command_number)
-));
