1317bd6d477a2fac61a61ba6150280ea792ea70c
James Waples <james@wapl.es>
2019-07-10 17:18:03 +0100

    Improve word() perf by ~30%

--- gcode-parser/src/word.rs
+++ gcode-parser/src/word.rs
@@ -1,7 +1,8 @@
 //! A single GCode literal like `G38.1` or `G0`
 
+use crate::parsers::char_no_case;
 use nom::{
-    bytes::complete::{is_a, tag_no_case},
+    bytes::complete::is_a,
     combinator::{recognize, verify},
     error::ParseError,
     sequence::pair,
@@ -13,11 +14,13 @@ pub fn word<'a, E: ParseError<&'a str>>(
     search: &'a str,
 ) -> impl Fn(&'a str) -> IResult<&'a str, &'a str, E> {
     let (letter, rest) = search.split_at(1);
+    let letter = letter.as_bytes()[0] as char;
+
     let padded = format!("0{}", rest);
 
     recognize(verify(
-        pair(tag_no_case(letter), is_a(".1234567890")),
-        move |(_, number): &(_, &str)| number == &rest || *number == padded,
+        pair(char_no_case(letter), is_a(".1234567890")),
+        move |(_, number): &(_, &str)| number == &rest || number == &padded,
     ))
 }
 
