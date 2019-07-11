d7337baf6a592b20abf16ad6614a4ac7f8e125fe
James Waples <james@wapl.es>
2019-07-11 10:43:49 +0100

    Optimise char_no_case parsing

--- gcode-parser/src/parsers.rs
+++ gcode-parser/src/parsers.rs
@@ -1,20 +1,25 @@
 //! Useful general purpose parsers
 
-use nom::{error::ParseError, AsChar, Err, IResult, InputIter, Slice};
+use nom::{error::ParseError, AsBytes, AsChar, Err, IResult, InputIter, Slice};
 use std::ops::RangeFrom;
 
-/// Like `tag_no_case`, but matches against a single char
+/// Like `tag_no_case`, but matches against a single char. Assumes input is ASCII as it operates on
+/// bytes, not characters
 pub fn char_no_case<I, Error: ParseError<I>>(c: char) -> impl Fn(I) -> IResult<I, char, Error>
 where
-    I: Slice<RangeFrom<usize>> + InputIter,
+    I: AsBytes + Slice<RangeFrom<usize>> + InputIter,
     <I as InputIter>::Item: AsChar,
 {
-    move |i: I| match (i).iter_elements().next().map(|t| {
-        let b = t.as_char().eq_ignore_ascii_case(&c);
-        (&c, b)
-    }) {
-        Some((c, true)) => Ok((i.slice(c.len()..), c.as_char())),
-        _ => Err(Err::Error(Error::from_char(i, c))),
+    move |i: I| {
+        if i.as_bytes()
+            .first()
+            .map(|f| f.eq_ignore_ascii_case(&(c as u8)))
+            .unwrap_or(false)
+        {
+            Ok((i.slice(1..), c.as_char()))
+        } else {
+            Err(Err::Error(Error::from_char(i, c)))
+        }
     }
 }
 
